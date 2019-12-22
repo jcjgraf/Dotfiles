4bae2a86ec5f5ef2e4edbab1cced9bad2854608f
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2019-12-22 20:02:04 +0100

    📦 NEW: Symlinker

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Variables
+dotfiles="$HOME"/Dotfiles
+
+# Delete existing dotfiles
+sudo rm -rf ~/.zshrc > /dev/null 2>&1
+sudo rm -rf ~/.config/i3 > /dev/null 2>&1
+sudo rm -rf ~/.config/i3blocks > /dev/null 2>&1
+sudo rm -rf ~/.config/i3scripts > /dev/null 2>&1
+sudo rm -rf ~/.config/termite > /dev/null 2>&1
+sudo rm -rf ~/.config/.Xresources > /dev/null 2>&1
+sudo rm -rf "~/.config/gtk-3.0" > /dev/null 2>&1
+sudo rm -rf "~/.gtkrc-2.0" > /dev/null 2>&1
+sudo rm -rf ~/.config/fontconfig > /dev/null 2>&1
+sudo rm -rf ~/.config/mimeapps.list > /dev/null 2>&1
+sudo rm -rf ~/.vimrc > /dev/null 2>&1
+sudo rm -rf ~/.config/redshift > /dev/null 2>&1
+
+
+sudo rm -rf /etc/acpi > /dev/null 2>&1
+
+# Create symlink
+ln -sf "$dotfiles/zsh/.zshrc" ~/.zshrc
+ln -sf "$dotfiles/wm/i3" ~/.config/i3
+ln -sf "$dotfiles/wm/i3blocks" ~/.config/i3blocks
+ln -sf "$dotfiles/wm/i3scripts" ~/.config/i3scripts
+ln -sf "$dotfiles/termite" ~/.config
+ln -sf "$dotfiles/.Xresources" ~/
+ln -sf "$dotfiles/gtk-3.0" ~/.config
+ln -sf "$dotfiles/.gtkrc-2.0" ~/
+ln -sf "$dotfiles/fontconfig" ~/.config
+ln -sf "$dotfiles/mimeapps.list" ~/.config
+ln -sf "$dotfiles/.vimrc" ~/
+ln -sf "$dotfiles/redshift" ~/.config
+
+
+sudo ln -sf $dotfiles/acpi /etc/acpi
