ba867ac4f068618a95eb3cd5667bfcd53ed04fce
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2020-01-16 22:54:54 +0100

    📦 NEW: Restructure installer and add a symlinker method

--- install.sh
+++ install.sh
@@ -1,46 +1,42 @@
 #!/bin/bash
+# Symlinkes dotfiles to correct location
 
-# Variables
-dotfiles="$HOME"/Dotfiles
+dotfiles=~/Dotfiles
 
-# Delete existing dotfiles
-sudo rm -rf ~/.zshrc > /dev/null 2>&1
-sudo rm -rf ~/.config/i3 > /dev/null 2>&1
-sudo rm -rf ~/.config/i3blocks > /dev/null 2>&1
-sudo rm -rf ~/.config/i3scripts > /dev/null 2>&1
-sudo rm -rf ~/.config/termite > /dev/null 2>&1
-sudo rm -rf ~/.config/.Xresources > /dev/null 2>&1
-sudo rm -rf "~/.config/gtk-3.0" > /dev/null 2>&1
-sudo rm -rf "~/.gtkrc-2.0" > /dev/null 2>&1
-sudo rm -rf ~/.config/fontconfig > /dev/null 2>&1
-sudo rm -rf ~/.config/mimeapps.list > /dev/null 2>&1
-sudo rm -rf ~/.vimrc > /dev/null 2>&1
-sudo rm -rf ~/.config/redshift > /dev/null 2>&1
-sudo rm -rf /etc/acpi > /dev/null 2>&1
-sudo rm -rf ~/.gitconfig > /dev/null 2>&1
-sudo rm -rf ~/bin/lock.sh > /dev/null 2>&1
-sudo rm -rf ~/.config/dunst > /dev/null 2>&1
-sudo rm -rf ~/.config/locale.conf > /dev/null 2>&1
-sudo rm -rf ~/.config/mpv > /dev/null 2>&1
+linker() {
+    # Symlinks file $1 to destination $2
+    ## Caution: destination $2 gets removes!
 
+    sudo rm -rf $2 > /dev/null 2>&1
+    ln -sf $1 $2
+}
 
-# Create symlink
-ln -sf "$dotfiles/zsh/.zshrc" ~/.zshrc
-ln -sf "$dotfiles/wm/i3" ~/.config/i3
-ln -sf "$dotfiles/wm/i3blocks" ~/.config/i3blocks
-ln -sf "$dotfiles/wm/i3scripts" ~/.config/i3scripts
-ln -sf "$dotfiles/termite" ~/.config
-ln -sf "$dotfiles/.Xresources" ~/
-ln -sf "$dotfiles/gtk-3.0" ~/.config
-ln -sf "$dotfiles/.gtkrc-2.0" ~/
-ln -sf "$dotfiles/fontconfig" ~/.config
-ln -sf "$dotfiles/mimeapps.list" ~/.config
-ln -sf "$dotfiles/.vimrc" ~/
-ln -sf "$dotfiles/redshift" ~/.config
-sudo ln -sf $dotfiles/acpi /etc/acpi
-ln -sf "$dotfiles/.gitconfig" ~/
-ln -sf "$dotfiles/Scripts/lock.sh" ~/bin/lock.sh
-ln -sf "$dotfiles/dunst" ~/.config/dunst
-ln -sf "$dotfiles/locale.conf" ~/.config/locale.conf
-ln -sf "$dotfiles/mpv" ~/.config/mpv
+# Link Files
+## System
+linker "$dotfiles/zsh/.zshrc" ~/.zshrc
+linker "$dotfiles/termite" ~/.config/termite
+linker "$dotfiles/.Xresources" ~/.Xresources
+linker "$dotfiles/gtk-3.0" ~/.config/gtk-3.0
+linker "$dotfiles/.gtkrc-2.0" ~/.gtkrc-2.0
+linker "$dotfiles/fontconfig" ~/.config/fontconfig
+linker "$dotfiles/mimeapps.list" ~/.config/mimeapps.list
+linker "$dotfiles/locale.conf" ~/.config/locale.conf
+linker "$dotfiles/Scripts/lock.sh" ~/bin/lock.sh
+sudo bash -c "$(declare -f linker); linker \"$dotfiles/acpi\" '/etc/acpi'" # Run as root
 
+## WM
+linker "$dotfiles/wm/i3" ~/.config/i3
+linker "$dotfiles/wm/i3blocks" ~/.config/i3blocks
+linker "$dotfiles/wm/i3scripts" ~/.config/i3scripts
+
+## Applications
+linker "$dotfiles/.vimrc" ~/.vimrc
+linker "$dotfiles/redshift" ~/.config/redshift
+linker "$dotfiles/.gitconfig" ~/.gitconfig
+linker "$dotfiles/dunst" ~/.config/dunst
+linker "$dotfiles/mpv" ~/.config/mpv
+
+## Mail
+linker "$dotfiles/Mail/mutt" ~/.config/mutt
+linker "$dotfiles/Mail/msmtp" ~/.config/msmtp
+linker "$dotfiles/Mail/.mbsyncrc" ~/.mbsyncrc
