90edd7a69f97c868be0dc75e8e62e82b78a089da
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2020-03-01 10:04:35 +0100

    IMPROVE: Correct brightness levels and add notification

--- Scripts/backlight.sh
+++ Scripts/backlight.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Control the backlight brightness
 
+sendNotification=false
+
 currentLevelCache=~/.cache/backlight/level
 backlightDev=/sys/class/backlight/intel_backlight/
 
-declare -a levels=(0 1000 2000)
+declare -a levels=(0 50 100 200 500 1000 2000 3500 5000 7500 10000 15000 20000 24242)
 defaultValue=2
 
 getCurrentLevel() {
@@ -12,7 +14,7 @@ getCurrentLevel() {
 
     if [[ -f $currentLevelCache ]]
     then
-        echo $(<$currentLevelCache) 
+        echo $(<$currentLevelCache)
     else
         mkdir -p "$(dirname $currentLevelCache)"
         echo "$defaultValue" > $currentLevelCache
@@ -22,41 +24,64 @@ getCurrentLevel() {
 changeLevel() {
     # Change the brightness level according the the provided $1. + for increase, - for decrease
 
-    currentLevel=$(getCurrentLevel)   
+    local currentLevel=$(getCurrentLevel)
 
     case "$1" in
         +)
-            upperBound="${#levels[@]}"
-            newLevel=$(( currentLevel + 1 < upperBound ? currentLevel + 1 : currentLevel ))
+            local upperBound="${#levels[@]}"
+            local newLevel=$(( currentLevel + 1 < upperBound ? currentLevel + 1 : currentLevel ))
             ;;
         -)
-            newLevel=$(( currentLevel - 1 >= 0 ? currentLevel - 1 : 0 ))
+            local newLevel=$(( currentLevel - 1 >= 0 ? currentLevel - 1 : 0 ))
             ;;
         *)
-            newLevel=$currentLevel
+            local newLevel=$currentLevel
             ;;
     esac
-    
+
     echo $newLevel > $currentLevelCache
     echo ${levels[$newLevel]} > "${backlightDev}brightness"
+}
 
+sendNotification() {
+    local currentLevel=$(getCurrentLevel)
+    local maxLevel="${#levels[@]}"
+    local levelDiff=$((maxLevel - currentLevel - 1))
+
+    local barLeft=$(seq -s "=" 0 $currentLevel | sed 's/[0-9]//g')
+    local barRight=$(seq -s "-" 0 $levelDiff | sed 's/[0-9]//g')
+
+    notifier -a "changeBacklight" -u low -r "1234212" "${barLeft}${barRight}"
 }
 
 usage() {
-    echo "Usage: $0\n-i Increase brighness\n-d decrease brightness\n-h help"
+    printf "Usage: $0\n-i Increase brighness\n-d decrease brightness\n-n notification\n-h help"
 }
 
-while getopts 'idh' flag
+while getopts 'idhn' flag
 do
     case "${flag}" in
         i)
-            echo "$(changeLevel +)"
+            changeState=+
             ;;
         d)
-            echo "$(changeLevel -)"
+            changeState=-
+            ;;
+        n)
+            sendNotification=true
             ;;
         h | *)
             usage
             exit 1 ;;
     esac
 done
+
+if [[ -n ${changeState+x} ]]
+then
+    echo "$(changeLevel $changeState)"
+fi
+
+if [[ $sendNotification = true ]]
+then
+    $(sendNotification)
+fi
