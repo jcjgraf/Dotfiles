7cd278d7b7da1060094f3671d2ca75ca7ca2e234
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2020-03-05 21:31:16 +0100

    NEW: Add flags for basic i3lock options

--- Scripts/lock.sh
+++ Scripts/lock.sh
@@ -1,21 +1,70 @@
 #!/bin/sh
 
-# Prepare background
-imgPath="/tmp/bg_img.png"
+isAutoDim=false
+addImage=false
+isNoFork=false
 
-# Take Screenshot
-#scrot --multidisp --quality 50 --overwrite --silent "$imgPath"
+args="--ignore-empty-password"
 
-# Pixelate Image
-#convert -scale 10% -scale 1000% "$imgPath" "$imgPath"
+usage() {
+    printf "Usage: $0\n-i add image\n-d enable auto dim\n-f enable no fork\n-h help"
+}
 
-# Blure Image
-#ffmpeg -y -i "$imgPath" -filter_complex "boxblur=5:5" -loglevel quiet "${imgPath}_out.png" && mv "${imgPath}_out.png" "$imgPath"
+while getopts 'idfh' flag
+do
+    case "${flag}" in
+        i)
+            addImage=true
+            ;;
+        d)
+            isAutoDim=true
+            ;;
+        f)
+           isNoFork=true 
+            ;;
+        h | *)
+            usage
+            exit 1 ;;
+    esac
+done
 
-# Screen setting and lock
-set -e
-xset s off dpms 0 3 0
-#i3lock --ignore-empty-password --nofork --tiling --image "$imgPath"
-color="$( xrdb -query | grep background: | awk '{print $NF}' )"
-i3lock --ignore-empty-password --nofork --color $color 
-xset s off -dpms
+# Add background image or color
+if [[ $addImage = true ]]
+then
+    imgPath="/tmp/bg_img.png"
+
+    scrot --multidisp --quality 50 --overwrite --silent "$imgPath"
+
+    # Pixelate Image
+    #convert -scale 10% -scale 1000% "$imgPath" "$imgPath"
+
+    # Blure Image
+    ffmpeg -y -i "$imgPath" -filter_complex "boxblur=5:5" -loglevel quiet "${imgPath}_out.png" && mv "${imgPath}_out.png" "$imgPath"
+
+    args="${args} --tiling --image \"$imgPath\""
+else
+    color="$( xrdb -query | grep background: | awk '{print $NF}' )"
+    args="${args} --color $color"
+fi
+
+# Dim screen when locked
+if [[ $isAutoDim = true ]]
+then
+    set -e
+    xset s off dpms 0 3 0
+fi
+
+# Add nofork option 
+if [[ $isNoFork = true ]]
+then
+    args="${args} --nofork"
+fi
+
+i3lock $args
+
+# Undim screen
+
+if [[ $isAutoDim = true ]]
+then
+    xset s off -dpms
+fi
