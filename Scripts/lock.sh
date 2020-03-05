8262fa7571cbb8309c42f7c10e2201b3efe7f9e2
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2020-03-05 20:41:41 +0100

    IMPROVE: Add system background color to the lock screen

--- Scripts/lock.sh
+++ Scripts/lock.sh
@@ -4,10 +4,10 @@
 imgPath="/tmp/bg_img.png"
 
 # Take Screenshot
-scrot --multidisp --quality 50 --overwrite --silent "$imgPath"
+#scrot --multidisp --quality 50 --overwrite --silent "$imgPath"
 
 # Pixelate Image
-convert -scale 10% -scale 1000% "$imgPath" "$imgPath"
+#convert -scale 10% -scale 1000% "$imgPath" "$imgPath"
 
 # Blure Image
 #ffmpeg -y -i "$imgPath" -filter_complex "boxblur=5:5" -loglevel quiet "${imgPath}_out.png" && mv "${imgPath}_out.png" "$imgPath"
@@ -15,5 +15,7 @@ convert -scale 10% -scale 1000% "$imgPath" "$imgPath"
 # Screen setting and lock
 set -e
 xset s off dpms 0 3 0
-i3lock --ignore-empty-password --nofork --tiling --image "$imgPath"
+#i3lock --ignore-empty-password --nofork --tiling --image "$imgPath"
+color="$( xrdb -query | grep background: | awk '{print $NF}' )"
+i3lock --ignore-empty-password --nofork --color $color 
 xset s off -dpms
