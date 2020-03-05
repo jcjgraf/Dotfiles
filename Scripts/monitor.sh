61a0f01b3d8a2f58b93add2158c8ad9c922ad6f8
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2020-03-05 09:47:29 +0100

    IMPROVE: Map touch devices to built-in after applying changes

--- Scripts/monitor.sh
+++ Scripts/monitor.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Monitor controller
 
+penMap() {
+    xinput --map-to-output 'Wacom Pen and multitouch sensor Finger touch' eDP-1
+    xinput --map-to-output 'Wacom Pen and multitouch sensor Pen stylus' eDP-1
+    xinput --map-to-output 'Wacom Pen and multitouch sensor Pen eraser' eDP-1
+}
+
 usage() {
     echo "Usage: $0 \n
         -h help\n
@@ -22,14 +28,17 @@ do
         s)
             # Single monitor built-in
             xrandr --output eDP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off
+            penMap
             ;;
         e)
             # Single monitor external
             xrandr --output eDP-1 --off  --output DP-1 --off --output HDMI-1 --primary --mode 1920x1080 --scale 2.5x2.5 --rotate normal --output DP-2 --off
+            penMap
             ;;
         m)
             # Multiple monitor
             xrandr --output eDP-1 --primary --mode 3840x2160 --pos 432x2700 --rotate normal --output DP-1 --off --output HDMI-1 --mode 1920x1080 --scale 2.5x2.5 --pos 0x0 --rotate normal --output DP-2 --off
+            penMap
             ;;
         h | *)
             usage
