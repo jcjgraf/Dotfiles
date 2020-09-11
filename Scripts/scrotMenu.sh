960a218ee08fbaa8622f744e2760cab50cef61c1
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2020-09-11 23:20:56 +0200

    NEW: Rofi scrot menu

--- Scripts/scrotMenu.sh
+++ Scripts/scrotMenu.sh
@@ -7,7 +7,7 @@ o3="Window"
 
 options="$o0\n$o1\n$o2\n$o3"
 
-selection="$(echo -e "$options" | displayer rofi -lines 4 -dmenu -p "Scrot" -a 0 -no-sustom | awk '{print $1}')"
+selection="$(echo -e "$options" | displayer rofi -lines 4 -dmenu -p "Scrot" -a 0 -no-sustom)"
 
 case $selection in
     $o1)
