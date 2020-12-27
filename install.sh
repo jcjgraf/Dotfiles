103fcb74582bf3a2102ba3567b8df6833c22f6f5
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2020-12-27 09:21:43 +0100

    RESTORE: move GPG back to the home directory
    Moving it to the XDG dir has lead to problems

--- install.sh
+++ install.sh
@@ -196,7 +196,7 @@ then
     linker "$dotfiles/Newsboat" ~/.config/newsboat
     linker "$dotfiles/Rofi" ~/.config/rofi
     linker "$dotfiles/Wget" ~/.config/wget
-    linker "$dotfiles/Gnupg/gpg-agent.conf" ~/.local/share/gnupg/gpg-agent.conf
+    linker "$dotfiles/Gnupg/gpg-agent.conf" ~/./gnupg/gpg-agent.conf
 fi
 
 ## Mail
