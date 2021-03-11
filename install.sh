48673166af08591fea763d523cfdad461d4f1e92
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2021-03-11 17:40:34 +0100

    IMPROVE: Taskwarrior color theme

--- install.sh
+++ install.sh
@@ -198,6 +198,7 @@ then
     linker "$dotfiles/Wget" ~/.config/wget
     linker "$dotfiles/Gnupg/gpg-agent.conf" ~/.gnupg/gpg-agent.conf
     linker "$dotfiles/Taskwarrior/taskrc" ~/.config/taskwarrior/taskrc
+    linker "$dotfiles/Taskwarrior/dark-16.theme" ~/.config/taskwarrior/dark-16.theme
     linker "$dotfiles/Taskwarrior/on-modify.timewarrior" ~/.local/share/taskwarrior/hooks/on-modify.timewarrior
     linker "$dotfiles/Timewarrior/timewarrior.cfg" ~/.config/timewarrior/timewarrior.cfg
 fi
