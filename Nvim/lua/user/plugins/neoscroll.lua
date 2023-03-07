f8d9faf2c0c4d8707ab804985c53d293a411ff76
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2023-03-07 22:59:36 +0100

    Improve scrollspeed

--- Nvim/lua/user/plugins/neoscroll.lua
+++ Nvim/lua/user/plugins/neoscroll.lua
@@ -3,8 +3,8 @@ require('neoscroll').setup({
 })
 
 require('neoscroll.config').set_mappings({
-    ['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '150' } },
-    ['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '150' } },
+    ['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '200' } },
+    ['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '200' } },
     ['<C-b>'] = { 'scroll', { '-vim.api.nvim_win_get_height(0)', 'true', '350' } },
     ['<C-f>'] = { 'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '350' } },
     ['<C-y>'] = { 'scroll', { '-0.10', 'false', '100' } },
