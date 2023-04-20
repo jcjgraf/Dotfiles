fb080a369c31e96a650e736def4ac22d6784b52b
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2023-04-20 16:41:11 +0200

    Improve calendars by adding a merged calendar
    The sync.sh script is responsible for getting all data from the
    different sources, and synchronizes them. In addition, it crates two
    merged calendars and synchronizes them as well.

---
+++ Calendar/sync.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Enable globs
+shopt -s extglob
+
+calendarPath="/home/jeanclaude/.local/share/calendar/"
+
+# Get from singlefile
+vdirsyncer sync calendars_to_singlefile
+
+# Get from remote
+vdirsyncer sync calendars_to_remote
+
+# Push to singlefile
+vdirsyncer sync calendars_to_singlefile
+
+# Generate merged
+rm -fr "${calendarPath}/merged/personal"
+rm -fr "${calendarPath}/merged/studies"
+
+mkdir -p "${calendarPath}/merged/personal"
+mkdir -p "${calendarPath}/merged/studies"
+
+cp "${calendarPath}/regular/personal_"*"/"* "${calendarPath}/merged/personal"
+cp "${calendarPath}/regular/studies_"*"/"* "${calendarPath}/merged/studies"
+
+# Push merged
+vdirsyncer sync calendars_merged_to_remote
+
+# Sync Calendar
+vdirsyncer sync calendar
