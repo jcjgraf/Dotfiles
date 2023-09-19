131b0a87e329123f81269b3f94ed9d8c67d636a6
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2023-09-19 08:34:04 +0200

    Add print statements
    Makes debugging easier

--- CalCard/sync.sh
+++ CalCard/sync.sh
@@ -6,15 +6,19 @@ shopt -s extglob
 calendarPath="/home/jeanclaude/.local/share/calendar/"
 
 # Get from singlefile
+echo "Sync singlefile"
 vdirsyncer --config /home/jeanclaude/Dotfiles/CalCard/VdirSyncer/config sync calendars_to_singlefile
 
 # Get from remote
+echo "Sync remote"
 vdirsyncer --config /home/jeanclaude/Dotfiles/CalCard/VdirSyncer/config sync calendars_to_remote
 
 # Push to singlefile
+echo "Sync singlefile"
 vdirsyncer --config /home/jeanclaude/Dotfiles/CalCard/VdirSyncer/config sync calendars_to_singlefile
 
 # Generate merged
+echo "Create merged"
 rm -fr "${calendarPath}/merged/personal"
 rm -fr "${calendarPath}/merged/studies"
 
@@ -25,7 +29,9 @@ cp "${calendarPath}/regular/personal_"*"/"* "${calendarPath}/merged/personal"
 cp "${calendarPath}/regular/studies_"*"/"* "${calendarPath}/merged/studies"
 
 # Push merged
+echo "Sync remote"
 vdirsyncer --config /home/jeanclaude/Dotfiles/CalCard/VdirSyncer/config sync calendars_merged_to_remote
 
 # Sync Calendar
+echo "Sync singlefile"
 vdirsyncer --config /home/jeanclaude/Dotfiles/CalCard/VdirSyncer/config sync contacts
