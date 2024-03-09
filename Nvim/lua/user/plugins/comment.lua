2d4f7f62d9441e57d5ed9f602d273068f9ff9972
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2024-03-09 17:37:24 +0100

    Change comment maps to not conflict with lsp maps
    Change comments to ml and mb.

--- Nvim/lua/user/plugins/comment.lua
+++ Nvim/lua/user/plugins/comment.lua
@@ -1,5 +1,28 @@
-require("Comment").setup()
+local comment = require("Comment")
 
-local ft = require('Comment.ft')
+comment.setup({
+	---LHS of toggle mappings in NORMAL mode
+	toggler = {
+		---Line-comment toggle keymap
+		line = "ml",
+		---Block-comment toggle keymap
+		block = "mb",
+	},
+	---LHS of operator-pending mappings in NORMAL and VISUAL mode
+	opleader = {
+		---Line-comment keymap
+		line = "ml",
+		---Block-comment keymap
+		block = "mb",
+	},
+	mappings = {
+		---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+		basic = true,
+		---Extra mapping; `gco`, `gcO`, `gcA`
+		extra = false,
+	},
+})
 
-ft.set('asm', '#%s')
+local ft = require("Comment.ft")
+
+ft.set("asm", "#%s")
