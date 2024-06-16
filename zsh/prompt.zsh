968f7b0ee64999cc6515e0b2fd5e9aab52fadd7f
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2024-06-16 19:54:31 +0200

    Remove git info due to slowdown

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,22 +1,22 @@
 setopt prompt_subst # Enable parameter expansion, command substitution, and arithmetic expansion
 autoload -Uz vcs_info
 
-zstyle ':vcs_info:*' enable git                                                           # Enable for git
-zstyle ':vcs_info:*' stagedstr 'M'                                                        # Char replaced by %c. If there are staged changes
-zstyle ':vcs_info:*' unstagedstr 'M'                                                      # Char replaced by %u. If there are unstaged changed
-zstyle ':vcs_info:*' check-for-changes true                                               # Enable check for staged and unstaged files
-zstyle ':vcs_info:*' actionformats '%F{green}[%F{green}%b%F{blue}|%F{red}%a%F{green}]%f ' # Formatting during action (like rebase)
-zstyle ':vcs_info:*' formats '%F{green}[%F{blue}%b%F{green}] %F{green}%c%F{yellow}%u%f'   # General formatting
+# zstyle ':vcs_info:*' enable git                                                           # Enable for git
+# zstyle ':vcs_info:*' stagedstr 'M'                                                        # Char replaced by %c. If there are staged changes
+# zstyle ':vcs_info:*' unstagedstr 'M'                                                      # Char replaced by %u. If there are unstaged changed
+# zstyle ':vcs_info:*' check-for-changes true                                               # Enable check for staged and unstaged files
+# zstyle ':vcs_info:*' actionformats '%F{green}[%F{green}%b%F{blue}|%F{red}%a%F{green}]%f ' # Formatting during action (like rebase)
+# zstyle ':vcs_info:*' formats '%F{green}[%F{blue}%b%F{green}] %F{green}%c%F{yellow}%u%f'   # General formatting
 
-zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
+# zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
 
-# Deal with untracked files
-+vi-git-untracked() {
-    if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && [[ $(git ls-files --other --directory --exclude-standard | sed q | wc -l | tr -d ' ') == 1 ]]
-        then
-        hook_com[unstaged]+='%F{red}??%f'
-    fi
-}
+# # Deal with untracked files
+# +vi-git-untracked() {
+#     if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && [[ $(git ls-files --other --directory --exclude-standard | sed q | wc -l | tr -d ' ') == 1 ]]
+#         then
+#         hook_com[unstaged]+='%F{red}??%f'
+#     fi
+# }
 
 precmd () { vcs_info }
 
