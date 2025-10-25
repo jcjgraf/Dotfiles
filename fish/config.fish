98871f91e3e4cee83b6736346054ae566b50ead2
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2025-10-25 22:32:47 +0200

    Add functionality for !! and !$

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,28 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    # From https://superuser.com/a/944589/673743
+    function bind_bang
+        switch (commandline -t)[-1]
+            case "!"
+                commandline -t -- $history[1]
+                commandline -f repaint
+            case "*"
+                commandline -i !
+        end
+    end
+
+    function bind_dollar
+        switch (commandline -t)[-1]
+            case "!"
+                commandline -f backward-delete-char history-token-search-backward
+            case "*"
+                commandline -i '$'
+        end
+    end
+
+    function fish_user_key_bindings
+        bind ! bind_bang
+        bind '$' bind_dollar
+    end
 end
