007420a10ed917b27291f4b540aa99f16409e6fb
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2025-11-20 08:50:47 +0100

    Conditionally use nvim, vim or vi for vim and vi command
    This allows the alias to be used also on systems where nvim is not
    installed.

--- fish/functions/vim.fish
+++ fish/functions/vim.fish
@@ -1,3 +1,20 @@
-function vim --wraps=nvim --description 'alias vim=nvim'
-    nvim $argv
+function __vim_fallback
+    if type -q nvim
+        command nvim $argv
+    else if type -q vim
+        command vim $argv
+    else if type -q vi
+        command vi $argv
+    else
+        echo "Error: no editor (nvim/vim/vi) found." >&2
+        return 1
+    end
+end
+
+function vim --description 'Use nvim if available, else vim, else vi'
+    __vim_fallback $argv
+end
+
+function vi --description 'Use nvim if available, else vim, else vi'
+    __vim_fallback $argv
 end
