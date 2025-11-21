75508964b563e01a96ee01d7e15e74cf6e8678e2
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2025-11-21 17:47:26 +0100

    Add alias to quickly go up in the file tree

---
+++ fish/functions/dots.fish
@@ -0,0 +1,11 @@
+function ... --wraps=cd --description 'alias cd ../..'
+     cd ../..
+end
+
+function .... --wraps=cd --description 'alias cd ../../..'
+     cd ../../..
+end
+
+function ..... --wraps=cd --description 'alias cd ../../../..'
+     cd ../../../..
+end
