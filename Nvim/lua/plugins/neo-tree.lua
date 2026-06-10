d0a4fccaeb1a1bab44cb348e3688f2c0a3a3fb2a
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2026-06-10 22:16:00 +0200

    Update config

--- Nvim/lua/plugins/neo-tree.lua
+++ Nvim/lua/plugins/neo-tree.lua
@@ -1,30 +1,32 @@
 -- File browser/tree
 
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
-		"nvim-tree/nvim-web-devicons",
-	},
-	keys = {
-		{ "<leader>ee", "<cmd>Neotree toggle<cr>", desc = "Toggle Neotree" },
-		{ "<leader>er", "<cmd>Neotree reveal<cr>", desc = "Show current file in Neotree" },
-	},
-	opts = {
-		filesystem = {
-			filtered_items = {
-				visible = true,
-				show_hidden_count = true,
-				hide_dotfiles = false,
-				hide_gitignored = true,
-				hide_by_name = {
-					-- '.git',
-					-- '.DS_Store',
-					-- 'thumbs.db',
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons", -- optional, but recommended
+		},
+		lazy = false, -- neo-tree will lazily load itself
+		keys = {
+			{ "<leader>ee", "<cmd>Neotree toggle<cr>", desc = "Toggle Neotree" },
+			{ "<leader>er", "<cmd>Neotree reveal<cr>", desc = "Show current file in Neotree" },
+		},
+		opts = {
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					show_hidden_count = true,
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					-- 				hide_by_name = {
+					-- 					-- '.git',
+					-- 					-- '.DS_Store',
+					-- 					-- 'thumbs.db',
 				},
-				never_show = {},
+				-- 				never_show = {},
 			},
 		},
 	},
